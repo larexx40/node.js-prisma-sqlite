08cfcac79db2bb7c88e748a14189a958baf80f95
larexx40 <larexx40@gmail.com>
2022-05-07 18:19:22 +0100

    send mail with NodeMailer

---
+++ src/services/nodemailerService.js
@@ -0,0 +1,62 @@
+require('dotenv').config();
+const  nodemailer = require('nodemailer');
+
+//initialize nodemailer transporter
+let transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: process.env.GOOGLE_USER,
+        pass: process.env.GOOGLE_PASS
+    }
+})
+
+
+//reset password mail option
+const resetPasswordMail= async(username, email, token)=>{
+    const mailOptions={
+        from: '"Lanre" <lanretunji47@gmail.com>',
+        to: email,//user.email
+        subject: 'Reset Password',
+        text: `Hello ${username} use this ${token} to reset your password`
+    }
+
+    transporter.sendMail(mailOptions, (err, response)=>{
+        if (err){
+            throw err
+        }else{
+            console.log("Message sent successfully");
+            return{
+                msg: "Mail Sent",
+                status: 200,
+                res: response
+            }
+        }
+    })
+}
+
+const resetPasswordSuccessMail = async(username, email)=>{
+    const mailOptions={
+        from: '"Lanre" <lanretunji47@gmail.com>',
+        to: email,
+        subject: 'Reset Password',
+        text: `Password reset successful`
+    }
+
+    transporter.sendMail(mailOptions, (err, response)=>{
+        if (err){
+            throw err
+        }else{
+            console.log("Message sent successfully");
+            return{
+                msg: "Mail Sent",
+                status: 200,
+                res: response
+            }
+        }
+    })
+}
+
+module.exports = {
+    resetPasswordMail,
+    resetPasswordSuccessMail
+}
