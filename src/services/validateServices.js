217a65a7973aacba79e57203b7785c74ae0d0cb0
larexx40 <larexx40@gmail.com>
2022-05-27 14:50:07 +0100

    JOI validation services

---
+++ src/services/validateServices.js
@@ -0,0 +1,149 @@
+const Joi = require('joi');
+
+const validateSignupDetails = async (reqBody)=>{
+    let err = [];
+    const schema = Joi.object({
+        username: Joi.string()
+            .alphanum()
+            .min(8)
+            .max(30)
+            .required(),
+    
+        password: Joi.string()
+            .pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')).required(),
+    
+        email: Joi.string()
+            .email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }),
+        phoneNo: Joi.string().alphanum().required(),
+        name: Joi.string().required()
+    
+    }).required()
+    
+    const {username, email, name, phoneNo, password}= reqBody;
+    const { value, error } = schema.validate({username, email, name, phoneNo, password}, {abortEarly: false});
+    if (error) {
+        error.details.forEach(element => {
+            err.push(element.message)
+            console.log(element.message);
+        });
+        return {
+            validated: false,
+            err,
+            status: 400
+        }
+
+    }else {
+        return {
+            validated: true,
+            value,
+            status:200
+        }
+    }
+}
+
+const validateLoginDetails = async(reqBody)=>{
+    let err = []
+    const schema = Joi.object({    
+        email: Joi.string()
+            .email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }),
+        password: Joi.string()
+            .pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')).required()
+    
+    }).required()
+    
+    const {email, password}= reqBody;
+    const { value, error } = schema.validate({email, password}, {abortEarly: false});
+    
+    if (error) {
+        error.details.forEach(element => {
+            err.push(element.message)
+            console.log(element.message);
+        });
+        return {
+            validated: false,
+            err,
+            status: 400
+        }
+
+    }else {
+        return {
+            validated: true,
+            value,
+            status:200
+        }
+    }
+}
+
+const validatePassword = async (password)=>{
+    const schema = Joi.object().keys({
+        password: Joi.string()
+        .pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')).required(),
+    })
+    const {error, value} = schema.validate({password})
+    if (error) {
+        console.log("error", error)
+        return {
+            msg: "password input not valid",
+            error,
+            status: 403
+        }
+    }else {
+        return {
+            validated: true,
+            value,
+            status:200
+        }
+    }
+}
+
+const validateEmail = async(email)=>{
+    const schema = Joi.object().keys({
+        mail: Joi.string().email().required()
+    })
+    const {error, value} = schema.validate({email})
+    if (error) {
+        console.log("error", error)
+        return {
+            msg: "email input not valid",
+            error,
+            status: 403
+        }
+    }else {
+        return {
+            validated: true,
+            value,
+            status:200
+        }
+    }
+}
+//joi test 
+    const schema = Joi.object({
+        username: Joi.string().default('larry'),
+        firstname: Joi.string().required(),
+        lastname: Joi.string().required(),
+        password: Joi.string()
+        .pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')).required(),
+        created: Joi.date().default(Date.now),
+        status: Joi.string().default('registered')
+    }).required();
+    
+    const {error, value } = schema.validate({
+        firstname: 'Jane',
+    
+    }, { abortEarly: false });
+
+    if (error) {
+        error.details.forEach(element => {
+            console.log(element.message);
+        });
+    }else {
+        console.log('result', value);
+    }
+
+    module.exports  ={
+        validateSignupDetails,
+        validateLoginDetails,
+        validateEmail,
+        validatePassword
+    }
+    
